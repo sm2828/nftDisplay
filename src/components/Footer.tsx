5665807414965b1577c056345cb764b0248d71ba
monadbull <122620728+sm2828@users.noreply.github.com>
2026-08-12 11:47:05 -0500

    removed link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -130,17 +130,6 @@ const Footer: React.FC<FooterProps> = ({
               <h4 className="footer-heading">get in touch</h4>
               <div className="social-links" ref={socialLinksRef}>
                 <a 
-                  href="https://x.com/monadbull" 
-                  className="social-link" 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                  aria-label="X (Twitter)"
-                >
-                  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-                  </svg>
-                </a>
-                <a 
                   href="https://github.com/sm2828" 
                   className="social-link" 
                   target="_blank" 
